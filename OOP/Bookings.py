8f989f90b1b722b21a6cf725dc4c21aab59f23df
YeeKiatLim <116255011+YeeKiatLim@users.noreply.github.com>
2022-12-07 10:07:23 +0800

    Create Booking Object and Integrate to Booking Display

--- OOP/Bookings.py
+++ OOP/Bookings.py
@@ -1,14 +1,44 @@
-# class Bookings:
-    #def __init__(self,facility,date,timeslot):
-        #self.__facility=facility
-        #self.__date=date
-        #self.__timeslot=timeslot
+import random
 
-   #def set_facility(self,facility):
-        #self.__facility=facility
+class Bookings:
+     booking_id=""
+     def __init__(self,type):
+          self.__type=type
 
-   #def set_date(self,date):
-        #self.__date=date
+     def set_booking_id(self):
+          tempList = []
+          for i in range(7): # 7-Digit Random
+            randomID = random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890')
+            tempList.append(randomID)
+          self.__booking_id="".join(tempList)
 
-   #def set_timeslot(self,timeslot):
-        #self.__timeslot=timeslot
+     def get_booking_id(self):
+          return self.__booking_id
+
+     def get_type(self):
+          return self.__type
+     
+class FacilityBooking(Bookings):
+     def __init__(self,type,facility,date,timeslot):
+          Bookings.__init__(self,type)
+          self.__facility=facility
+          self.__date=date
+          self.__timeslot=timeslot
+
+     def set_facility(self,facility):
+          self.__facility=facility
+
+     def set_date(self,date):
+          self.__date=date
+
+     def set_timeslot(self,timeslot):
+          self.__timeslot=timeslot
+
+     def get_facility(self):
+          return self.__facility
+
+     def get_date(self):
+          return self.__date
+
+     def get_timeslot(self):
+          return self.__timeslot
